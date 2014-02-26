b1a26e8395e928e979871d25d185dbb231abf3b2
Cory Maughmer <cory.maughmer@neo.tamu.edu>
2014-02-26 01:47:07 -0600

    Rebuilt kinematics and functions for UI
    Redesigned the kinematic solver to include all links in bot to correctly
    produce movement and link positions. Redesign updateTheta and
    updateBaseSlider to return the list of joint positions for use by GUI
    and take input from Joint Control

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
 #include "forwardKinematics.h"
 
 int main(){
-	cout<<"Building the DH Parameters:: "<<endl;
-	DH_Param L1(0,0,0,90);
-	DH_Param L2(0,150,0,0);
-	DH_Param L3(0,100,0,0);
 
-	f_kin_solver_3dof solver(L1,L2,L3);
-	solver.printSolver();
+//----------------------------------------------------------------START INITIALIZE SOLVER-------------
 
-	cout<<"Changing Base Position and Recalculating:: "<<endl;
-	solver.updateBaseSlider(20);
-	solver.printSolver();
+// USE THIS BLOCK TO INIT THE SYSTEM FOR USE IN GUI
+	//Initializing the DH Parameters for each link
+	// (alpha, a, d, theta)
+	DH_Param L0(0,0,0,0);		// d is variable (prismatic)
+	DH_Param L1(90,150,0,0);	// non-variable transform
+	DH_Param L2(0,100,0,0);		// theta is variable (revolute)
+	DH_Param L3(0,75,0,0);		// theta is variable (revolute)
 
-	cout<<"Changing Theta2 and Recalculating:: "<<endl;
-	solver.updateTheta(2, 20);
-	solver.printSolver();
+	//Creating solver object and initializing positions
+	f_kin_solver solver(L0,L1,L2,L3);
+//------------------------------------------------------------------END INITIALIZE SOLVER-------------
+
+//-------------------------------------------------------------------START TESTING SOLVER-------------
+
+//USE updateBaseSlider(double) AND updateTheta(double, int) FUNCTIONS FOR JOINT CONTROL INTERFACE
+
+	solver.printLinkPoints();
+	//Testing movement of arm positions
+	cout<<"\nChanging Base Position and Recalculating:: \n";
+	solver.updateBaseSlider(75);
+	solver.printLinkPoints();
+	
+	cout<<"\nChanging Theta 2 and Recalculating:: \n";
+	solver.updateTheta(60, 2);
+	solver.printLinkPoints();
+	
+	cout<<"\nReseting Base Position and Recalculating:: \n";
+	solver.updateBaseSlider(0);
+	solver.printLinkPoints();
+
+	cout<<"\nReseting Theta 2 and Recalculating:: \n";
+	solver.updateTheta(0, 2);
+	solver.printLinkPoints();
+
+	cout<<"\nChanging Theta 3 and Recalculating:: \n";
+	solver.updateTheta(80, 3);
+	solver.printLinkPoints();
+
+//---------------------------------------------------------------------END TESTING SOLVER-------------
 	char hs=' ';
-	cout<<"Press any key + Enter to Exit:: ";
+	cout<<"\nPress any key + Enter to Exit:: ";
 	cin>>hs;
 	return 0;
 };
