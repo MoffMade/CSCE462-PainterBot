5687faa79b6fedfce5f77e69ad8f9d41f1133703
Cory Maughmer <cory.maughmer@neo.tamu.edu>
2014-03-18 05:20:28 -0500

    Rewrote inverse kinematic solution
    Used more robust law of cosine relations and d parameter method

--- PaintBot 2 - Inverse Kinematics/kinematics.cpp
+++ PaintBot 2 - Inverse Kinematics/kinematics.cpp
@@ -128,41 +128,77 @@ vector<vector<double> >  kin_solver::updateBaseSlider(double newSliderPos){
 	return XYpositions;
 };
 
-vector<vector<double> > kin_solver::inverseSolver(double end_x, double end_y, double phi){
+vector<vector<double> > kin_solver::inverseSolver(double end_x, double end_y){
 	//Change from graphics XY plane to ZX reference frame of robot
 	vector<vector<double> > linkPositions, XYpositions;
-	double theta2, theta3, d_length;
-	double z_end=end_x;
-	double x_end=end_y;
-	double l1, l2, l3;
-	l1=150;
-	l2=100;
-	l3=75;
-	//Determine wrist position
-	double z_wrist=z_end-l3*cos(phi);
-	double x_wrist=x_end-l3*sin(phi);
-	//Determine elbow position and theta2
-	double x_elbow=l1;
-	theta2=acos((x_wrist-l1)/l2);
-	d_length=z_wrist+l2*sin(theta2);
-	theta3=phi-theta2-90;
-	cout<<"Inverse solution- "<<"D: "<<d_length<<" theta2: "<<theta2<<" theta3: "<<theta3<<endl;
-	updateBaseSlider(d_length);
-	updateTheta(fmod(theta2,360),2);
-	XYpositions=updateTheta(fmod(theta3,360),3);
-	/*DH_Parameters[0].d=d_length;
-	DH_Parameters[2].theta=fmod(theta2,360);
-	DH_Parameters[3].theta=fmod(theta3,360);
-	updateTMatrices();
-	linkPositions=updateArmLinks();		//Get (X,Y,Z) positions for link points.
-	vector<double> temp;
-	//Change the points from (X,Y,Z) to (Z, X) points for use in the GUI XY plane
-	for(int i=0; i<linkPositions.size(); i++){
-		temp.clear();
-		temp.push_back(linkPositions[i][2]);	//Z value of position to first parameter
-		temp.push_back(linkPositions[i][0]);	//X value of position to second parameter
-		XYpositions.push_back(temp);			//Store (Z,X) position to XYpositions to return to the GUI
-	}*/
+	//Check range:  (x-300)^2+(y-150)^2<=175^2 || x^2+(y-150)^2<=175^2 || (x>=0&&x<=300&&y>=-25&&y<=325))
+	if( pow((end_x-300),2)+pow((end_y-150),2)<=pow(175,2) || pow(end_x,2)+pow((end_y-150),2)<=pow(175,2) || (end_x>=0&&end_x<=300&&end_y>=-25&&end_y<=325)) {
+		double theta2, theta3, d_length;
+		//requested (z,x) position for end effector
+		double z_end=end_x;
+		double x_end=end_y;
+		//arm link lengths, used in calculations
+		double l1, l2, l3;
+		l1=150;
+		l2=100;
+		l3=75;
+		double curr_d=DH_Parameters[0].d;
+		//Find new d_length
+		double zt, zb, Rx, r, trans;
+		r=l2+l3;
+		zt=z_end;
+		// Ry is y component of vector from (d,l1) to the desired end position translated along z axis within the current reachable circle
+		double Ry=(x_end-l1);
+		// 1. Translate the zt variable along z axis from the desired position to the current reachable circle without moving d parameter
+		// 2. change d_length by magnitude of translation
+		if(zt>curr_d){
+			zb=curr_d+sqrt(pow(r,2)-pow(Ry,2));
+			while(zt>zb){
+				zt-=rand()%5+1;
+			}
+			trans=abs(z_end-zt);
+			d_length=curr_d+trans;
+		}
+		else if(zt<=curr_d){
+			zb=curr_d-sqrt(pow(r,2)-pow(Ry,2));
+			while(zt<zb){
+				zt+=rand()%5+1;
+			}
+			trans=abs(z_end-zt);
+			d_length=curr_d-trans;
+		}
+		
+		//Find theta3 using law of cosines between the end of the prismatic joint and the desired end effector position 
+		theta3=180-acos((pow(l2,2)+pow(l3,2)-pow(z_end-d_length,2)-pow(x_end-l1,2))/(2*l2*l3));
+		//Find theta2 by finding alpha across from the vector (d_length,0) to (z_end, x_end) in the triangle formed with L1 and the vector
+		// (d_length, L1) to (z_end, x_end) and beta across from L3 in the triangle formed with L2 and the vector (d_length, L1) to (z_end, x_end)
+
+		//vec is the magnitude of vector (d_length, L1) to (z_end, x_end)
+		double vec= sqrt(pow(z_end-d_length,2)+pow(x_end-l1,2));
+		double alpha=acos((pow(x_end-l1,2)+pow(l1,2)-pow(x_end,2))/(2*l1*vec));
+		double beta=acos((pow(vec,2)+pow(l2,2)-pow(l3,2))/(2*l2*vec));
+		theta2=180+beta-alpha;
+
+		//Shift into 0<=theta<360 range
+		/*if(theta2<0)
+			theta2=360+theta2;
+		if(theta3<0)
+			theta3=360+theta3;
+		*/	
+		cout<<"Inverse solution- "<<"D: "<<d_length<<" theta2: "<<theta2<<" theta3: "<<theta3<<endl;
+		
+		updateBaseSlider(d_length);
+		updateTheta(fmod(theta2,360),2);
+		XYpositions=updateTheta(fmod(theta3,360),3);
+	}
+	else{
+		cout<<"Invalid end effector position, must be within reachable workspace; no change in position."<<endl;
+		linkPositions.push_back(Link0.getStart());
+		linkPositions.push_back(Link1.getStart());
+		linkPositions.push_back(Link2.getStart());
+		linkPositions.push_back(Link3.getStart());
+		XYpositions=linkPositions;
+	}
 	return XYpositions;
 };
 
--- PaintBot 2 - Inverse Kinematics/kinematics.h
+++ PaintBot 2 - Inverse Kinematics/kinematics.h
@@ -8,58 +8,6 @@
 using namespace std;
 #ifndef KINEMATICS_H
 #define KINEMATICS_H
-/*double degree2rad(double degrees);
-double rad2degree(double rads);
-
-class DH_Param {
-	//Angles are sotred in radians, entered and displayed in degrees
-public:
-	double alpha;
-	double a;
-	double d;
-	double theta;
-	DH_Param(): alpha(0), a(0), d(0), theta(0){};
-	DH_Param(double al, double ai, double di, double th): alpha(degree2rad(al)), a(ai), d(di), theta(degree2rad(th)) {};
-	void printDH(){
-		cout<<"("<<rad2degree(alpha)<<", "<<a<<", "<<d<<", "<<rad2degree(theta)<<") "<<endl;
-	};
-};
-
-class armLink {
-	//Stores the position of the link in the original reference frame.
-private:
-	vector<double> start_position;	//Start position in frame {0}
-	vector<double> end_position;	//End position in frame {0}
-public:
-	armLink();
-	armLink(vector<double> start, vector<double> end);
-	vector<double> getStart(){return start_position;};
-	vector<double> getEnd(){return end_position;};
-	void setStart(vector<double> spos){start_position=spos;};
-	void setEnd(vector<double> epos){end_position=epos;};
-
-};
-class T_Matrix {
-	//Allows for creation of a transformation matrix from a set of DH parameters
-	//Contains helper functions for multiplying by another T Matrix and a column vector 
-private:
-	int origin_frame, destination_frame;
-	double matrix[4][4];
-public:
-	T_Matrix();
-	T_Matrix(int org, int des, DH_Param dh);
-	void calcMatrix(DH_Param dh);
-	double get(int r, int c) const;
-	int getOrg() const;
-	int getDes() const;
-	void setOrg(int nOrg){origin_frame=nOrg;};
-	void setDes(int nDes){destination_frame=nDes;};
-	int set(double val, int r, int c);
-	void T_Mat_multiply(const T_Matrix A, const T_Matrix B);
-	vector<double> pointTransform(vector<double> org_pos);
-	void printMatrix();
-};
-*/
 class kin_solver {
 	/*Stores T Matrix for each link to the next as well as each link in reference to frame {0}
 	Contains functions to change the prismatic joint's d variable and the revolute joint's theta variable
@@ -92,7 +40,7 @@ public:
 	vector<vector<double> > updateArmLinks();
 	vector<vector<double> > updateTheta(double newTheta, int sub); //Returns a 2D vector with a X,Y pair as each row.
 	vector<vector<double> > updateBaseSlider(double newSliderPos);
-	vector<vector<double> > inverseSolver(double end_x, double end_y, double phi); //Takes X and Y coord of requested end effector position and upadtes the DH parameters
+	vector<vector<double> > inverseSolver(double end_x, double end_y); //Takes X and Y coord of requested end effector position and upadtes the DH parameters
 };
 
 
