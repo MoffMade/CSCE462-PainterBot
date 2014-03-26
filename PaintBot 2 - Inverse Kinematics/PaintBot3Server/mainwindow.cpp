ba5f20bf2519919842111d05d71a5d90acc32cbb
TheDude <tbcreighton@neo.tamu.edu>
2014-03-25 22:38:50 -0500

    added nearly all empty functions to Server

--- PaintBot 2 - Inverse Kinematics/PaintBot3Server/mainwindow.cpp
+++ PaintBot 2 - Inverse Kinematics/PaintBot3Server/mainwindow.cpp
@@ -5,6 +5,8 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    m_paint_flag = false;
+    m_delay = 0;
     ui->setupUi(this);
     m_dialog = new Dialog(this);
     connect(m_dialog, SIGNAL(run(qint16)), this, SLOT(onRunSignal(qint16)));
@@ -39,4 +41,141 @@ void MainWindow::acceptConnection()
 void MainWindow::readReady()
 {
     //got reply from client
+}
+
+/***************************************************************/
+//update functions
+/***************************************************************/
+
+void MainWindow::linkUpdate(double z, double theta1, double theta2)
+{
+
+}
+
+void MainWindow::updateLink1(double z)
+{
+
+}
+
+void MainWindow::updateLink2(double theta)
+{
+
+}
+
+
+void MainWindow::updateLink3(double theta)
+{
+
+}
+
+void MainWindow::worldUpdate(double x, double y)
+{
+
+}
+
+void MainWindow::updateX(double x)
+{
+
+}
+
+void MainWindow::updateY(double y)
+{
+
+}
+
+void MainWindow::updateDelay(int seconds)
+{
+
+}
+
+/***************************************************************/
+//GUI widget Slots
+/***************************************************************/
+
+
+//link 1
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+}
+
+void MainWindow::on_horizontalSlider_sliderMoved(int position)
+{
+
+}
+
+//link 2
+void MainWindow::on_pushButton_6_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+
+}
+
+void MainWindow::on_horizontalSlider_2_sliderMoved(int position)
+{
+
+}
+
+//link 3
+void MainWindow::on_pushButton_8_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+
+}
+
+void MainWindow::on_horizontalSlider_3_sliderMoved(int position)
+{
+
+}
+
+//X position
+void MainWindow::on_pushButton_10_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_11_clicked()
+{
+
+}
+
+//Y position
+
+void MainWindow::on_pushButton_12_clicked()
+{
+
+}
+void MainWindow::on_pushButton_13_clicked()
+{
+
+}
+
+//paint flag
+void MainWindow::on_pushButton_clicked()
+{
+    if(ui->pushButton->isChecked()){
+        m_paint_flag = true;
+    }
+    else{
+        m_paint_flag = false;
+    }
+}
+
+//set delay
+void MainWindow::on_pushButton_4_clicked()
+{
+    m_delay = ui->lineEdit->text().toInt();
 }
--- PaintBot 2 - Inverse Kinematics/PaintBot3Server/mainwindow.h
+++ PaintBot 2 - Inverse Kinematics/PaintBot3Server/mainwindow.h
@@ -28,10 +28,45 @@ private:
     QTcpServer *m_server;
     QTcpSocket *m_socket;
     qint16 m_port_number;
+    int m_delay;
+    bool m_paint_flag;
+
+    //updates robot display based on link control changes
+    void linkUpdate(double z, double theta1, double theta2);
+
+    //updates dh-parameter values
+    void updateLink1(double z);
+    void updateLink2(double theta);
+    void updateLink3(double theta);
+
+    //updates robot display based on world control changes
+    void worldUpdate(double x, double y);
+
+    //updates end-effector x and y locations
+    void updateX(double x);
+    void updateY(double y);
+
+    //updates simulated delay
+    void updateDelay(int seconds);
 
 private slots:
     void acceptConnection();
     void readReady();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_horizontalSlider_sliderMoved(int position);
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_horizontalSlider_2_sliderMoved(int position);
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+    void on_horizontalSlider_3_sliderMoved(int position);
+    void on_pushButton_10_clicked();
+    void on_pushButton_11_clicked();
+    void on_pushButton_12_clicked();
+    void on_pushButton_13_clicked();
+    void on_pushButton_clicked();
+    void on_pushButton_4_clicked();
 };
 
 #endif // MAINWINDOW_H
